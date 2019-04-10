398556407018d2ed8ba5d4d23475b33b4fc142dc
Giorgi Robitashvili <giorobota@gmail.com>
2019-04-10 14:22:57 +0400

    added events groups single group pages

---
+++ scripts/event-participants-modal.js
@@ -0,0 +1,21 @@
+window.onload = function(){ 
+    var modal = document.getElementById("modal");
+    var closeBtn = document.getElementById("closemodal");
+    var openBtn = document.getElementById("openlist");
+    openBtn.onclick = function () {
+        modal.style.display = "block";
+    }
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    closeBtn.onclick = function () {
+        modal.style.display = "none";
+    }
+
+}
+
+
+
+
